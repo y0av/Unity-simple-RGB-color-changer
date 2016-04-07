cd8816665781711ef6af366f8b307955b1eba47d
y0av <mail.yoav@gmail.com>
2016-04-07 19:07:41 +0300

    initial commit

---
+++ RGBscript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RGBscript : MonoBehaviour {
+
+	const float speed = 0.1f;
+	float t = 0;
+	int i = 0;
+	Color[] colors = {  Color.white,
+						Color.red,
+						Color.green,
+						Color.blue,
+						Color.cyan,
+						Color.magenta,
+						Color.yellow,
+						Color.white };
+	
+	void Update () {
+		t+=Time.deltaTime*speed;
+		gameObject.GetComponent<SpriteRenderer>().color = Color.Lerp(colors[i], colors[i+1], t);
+
+		if (t>1) {
+			t=0;
+			i++;
+			if (i>=colors.Length-1) i=0;
+		}
+	}
+}
